9a70fe225e88b663ee8ca7c7ab950cc3cb85091e
Devanshu Gupta <devanshugupta935@gmail.com>
2024-05-07 19:55:51 +0530

    Created basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Navbar } from "./Components/Navbar";
+import { Home } from "./Components/Home";
+// import { Cart } from "./Components/Cart";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        { index: true, element: <Home /> },
+        // { path: "/cart", element: <Cart /> },
+      ]
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
